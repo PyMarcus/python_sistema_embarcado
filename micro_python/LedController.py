471737d1af884bb7a92cc76ac83425270becb28c
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2021-12-10 11:39:58 -0300

    Update LedController.py

--- micro_python/LedController.py
+++ micro_python/LedController.py
@@ -36,3 +36,52 @@ if __name__ == '__main__':  # devido aos recursos computacionais limitados 64KB 
         ControlaLeds.led_verde()
         ControlaLeds.led_amarelo()
         ControlaLeds.led_azul()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     """ Outras implementações que fiz posteriormente:<br>
+     import machine<br>
+
+# The LED is connected to our virtual pin Y12<br>
+y12 = machine.Pin('Y12')  # confere se há algo em Y12<br>
+<br>
+y12(0 if y12() else 1)  # se estiver desligado, liga<br>
+<br>
+<br>
+# configurar uma saída<br>
+from machine import Pin<br>
+from time import sleep_ms<br>
+<br>
+y12 = Pin('Y12', Pin.OUT)  # configura o y12 como saída de led<br>
+y12.on()<br>
+sleep_ms(2000)<br>
+y12.off()<br>
+<br>
+# push the USR button on the pyboard to flash the LEDs!<br>
+# configuração de entrada<br>
+<br>
+import time<br>
+from pyb import Switch, LED<br>
+while True:<br>
+    if Switch().value():  # switch é o botão, que troca o estado dependendo do valor, se nao preciona = 0, apaga<br>
+        LED(1).on()<br>
+        LED(2).on()<br>
+    else:<br>
+        LED(1).off()<br>
+        LED(2).off()<br>
+    time.sleep_ms(100)<br>"""
+
